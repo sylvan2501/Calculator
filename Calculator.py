2c74d74d7ab74582ac546f45ab026269098c9d21
Sylvan <senguo@sylvans-macbook-pro.local>
2022-12-31 10:41:37 -0800

    first python file commit

---
+++ Calculator.py
@@ -0,0 +1,43 @@
+class Calculator:
+    def __init__(self):
+        self.operator = None
+        self.first_num = None
+        self.second_num = None
+        self.result = None
+
+    def set_operator(self, operator):
+        self.operator = operator
+
+    def set_first_num(self, first_num):
+        self.first_num = first_num
+
+    def set_second_num(self, second_num):
+        self.second_num = second_num
+
+    def set_result(self, result):
+        self.result = result
+
+    def interface_prompt(self):
+        self.set_first_num(float(input('Please enter the first number: \n')))
+        self.set_second_num(float(input('Please enter the second number: \n')))
+        self.set_operator(input('Please enter the operator: \n'))
+        if self.operator == '+':
+            self.set_result(self.first_num + self.second_num)
+        elif self.operator == '-':
+            self.set_result(self.first_num - self.second_num)
+        elif self.operator == '*':
+            self.set_result(self.first_num * self.second_num)
+        elif self.operator == '/':
+            if self.second_num == 0:
+                print('Invalid result !!')
+                return
+            else:
+                self.set_result(self.first_num / self.second_num)
+        else:
+            print('invalid operator!!')
+            return
+        print(f'Your result is {self.result}')
+
+
+calculator = Calculator()
+calculator.interface_prompt()
